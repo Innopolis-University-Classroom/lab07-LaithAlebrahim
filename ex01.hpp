4dde7274d4f8310468319b636f9bf09a58c05e69
LaithAlebrahim <96999016+LaithAlebrahim@users.noreply.github.com>
2022-03-04 17:14:04 +0300

    Update ex01.hpp

--- ex01.hpp
+++ ex01.hpp
@@ -1,12 +1,11 @@
 template<typename T>
-void  multiples (T& sum, T x, int n)
+void  multiples<T> (T& sum, T x, T n)
 {
-    sum=1;
+        sum+=1;
 
     for(int i=1;i<=n;i++){
-        sum+=x*(i+1);
+        sum+=x*i;
     }
 
 
-  
 }
